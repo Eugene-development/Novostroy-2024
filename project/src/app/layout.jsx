8705c1d7f5db269e2aa30b47da2ec84a8c3475f7
Eugene Che <indexpro24@gmail.com>
2024-05-14 15:54:32 +0300

    c

--- project/src/app/layout.jsx
+++ project/src/app/layout.jsx
@@ -11,7 +11,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <head>{process.env.NODE_ENV === "production" && <></>}</head>
+      <head>
+        {process.env.NODE_ENV === "production" && (
+          <>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                      m[i].l=1*new Date();
+                      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                      (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+                      ym(87611228, "init", {
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true,
+                            webvisor:true
+                      });
+                    <noscript><div><img src="https://mc.yandex.ru/watch/87611228" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+                    `,
+              }}
+            />
+          </>
+        )}
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
