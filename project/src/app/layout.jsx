67cd74018563c461e30342b5cf80d9dc14072f2c
Eugene Che <indexpro24@gmail.com>
2024-05-14 15:00:13 +0300

    c

--- project/src/app/layout.jsx
+++ project/src/app/layout.jsx
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
+      <head>{process.env.NODE_ENV === "production" && <></>}</head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- project/src/app/test/page.jsx
+++ project/src/app/test/page.jsx
@@ -1,10 +1,9 @@
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 
 export default function page() {
-
   return (
     <>
       <h1 className={styles.dashboard}>Neumorphic buttons</h1>
     </>
-  )
+  );
 }
