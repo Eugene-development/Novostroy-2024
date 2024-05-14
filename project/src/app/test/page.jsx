8898414ed3bc78f393913c92d394f3273643464d
Eugene Che <indexpro24@gmail.com>
2024-05-14 14:24:54 +0300

    c

--- project/src/app/test/page.jsx
+++ project/src/app/test/page.jsx
@@ -1,9 +1,10 @@
 import styles from './styles.module.css'
 
 export default function page() {
+
   return (
-        <>
-<h1 className={styles.dashboard}>Neumorphic buttons</h1>
-</>
+    <>
+      <h1 className={styles.dashboard}>Neumorphic buttons</h1>
+    </>
   )
 }
