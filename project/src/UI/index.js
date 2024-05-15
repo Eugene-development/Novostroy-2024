faa0902f12b11fb4d2a02febadc9344f39343867
Eugene Che <indexpro24@gmail.com>
2024-05-15 21:42:03 +0300

    c

--- project/src/UI/index.js
+++ project/src/UI/index.js
@@ -1,3 +1,4 @@
 export { default as Banner } from "./header/Banner";
 export { default as Menu } from "./header/Menu";
 export { default as Footer } from "./footer";
+export { default as Slider } from "./main/Slider"
--- project/src/app/page.jsx
+++ project/src/app/page.jsx
@@ -1,3 +1,7 @@
+import { Slider } from "@/UI";
+
 export default function Home() {
-  return <p className="text-3xl font-bold underline">1bdgfgbsgbsgbsdgbsfdgb</p>;
+  return (
+    <Slider />
+  )
 }
