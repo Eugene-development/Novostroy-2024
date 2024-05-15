bd3163608c3aae5244da5e2f7a8c55b2b24bcd9a
Eugene Che <indexpro24@gmail.com>
2024-05-15 19:59:27 +0300

    c

--- project/src/UI/header/Menu/NavBar/index.jsx
+++ project/src/UI/header/Menu/NavBar/index.jsx
@@ -72,6 +72,14 @@ export default function Navbar({ className }) {
             <HoveredLink href="/enterprise">Enterprise</HoveredLink>
           </div>
         </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Блог">
+          <div className="flex flex-col space-y-4 text-sm">
+            <HoveredLink href="/hobby">Hobby</HoveredLink>
+            <HoveredLink href="/individual">Individual</HoveredLink>
+            <HoveredLink href="/team">Team</HoveredLink>
+            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+          </div>
+        </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Контакты">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/hobby">Hobby</HoveredLink>
