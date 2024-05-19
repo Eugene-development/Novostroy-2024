8fdbc7356512bf1bd53f07d5ee59d0bc0b451cb6
Eugene Che <indexpro24@gmail.com>
2024-05-19 21:05:04 +0300

    с

--- project/src/UI/main/Slider/index.jsx
+++ project/src/UI/main/Slider/index.jsx
@@ -9,7 +9,7 @@ export default function ImagesSliderDemo() {
         "https://zovofficial.com/image/cache/catalog/products/korpusnaya-mebel/5-pemont/gostinaya/2-1600x942.jpg",
     ];
     return (
-        <ImagesSlider className="h-[50rem]" images={images}>
+        <ImagesSlider className="h-[51rem]" images={images}>
             <motion.div
                 initial={{
                     opacity: 0,
@@ -24,11 +24,11 @@ export default function ImagesSliderDemo() {
                 }}
                 className="z-50 flex flex-col justify-center items-center"
             >
-                <motion.p className="font-bold text-xl md:text-8xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+                <motion.p className="font-bold text-xl md:text-8xl text-center bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-400 py-4">
                     Мы делаем  <br />ваш дом немного лучше
                 </motion.p>
                 <button className="px-8 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
-                    <span>Посмотреть каталог →</span>
+                    <span> Подробнее →</span>
                     <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
                 </button>
             </motion.div>
