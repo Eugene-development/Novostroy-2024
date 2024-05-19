8c483a2b72ef989192e6b1a18d9bd155508d4556
Eugene Che <indexpro24@gmail.com>
2024-05-19 19:22:25 +0300

    c

--- project/src/UI/main/ThreeDCard/index.jsx
+++ project/src/UI/main/ThreeDCard/index.jsx
@@ -25,7 +25,7 @@ export default function ThreeDCardDemo() {
                     </CardItem>
                     <CardItem translateZ="100" className="w-full mt-4">
                         <Image
-                            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                            src="https://storage.yandexcloud.net/mine2024/zov/4DCcVnbk5suIuE8pD7yLzC6CUReEjgNJZkD2c32S.png"
                             height="1000"
                             width="1000"
                             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
