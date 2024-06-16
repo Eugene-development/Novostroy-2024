6ebf0d18ea7bbadcb87eca3ad33bed21ebcb9239
Eugene Che <indexpro24@gmail.com>
2024-06-16 08:36:17 +0300

    с

--- project/src/UI/header/Menu/NavBar/index.jsx
+++ project/src/UI/header/Menu/NavBar/index.jsx
@@ -33,25 +33,25 @@ export default function Navbar({ className }) {
           <div className="text-sm grid grid-cols-2 gap-10 p-8 bg-gray-100 rounded-xl">
             <ProductItem
               title="Мебель"
-              href="https://algochurn.com"
+              href="/catalog"
               src="https://assets.aceternity.com/demos/algochurn.webp"
               description="Production ready Tailwind css components for your next project"
             />
             <ProductItem
               title="Бытовая Техника"
-              href="https://tailwindmasterkit.com"
+              href="/catalog"
               src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
               description="Production ready Tailwind css components for your next project"
             />
             <ProductItem
               title="Столешницы"
-              href="https://gomoonbeam.com"
+              href="/catalog"
               src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
               description="Never write from scratch again. Go from idea to blog in minutes."
             />
             <ProductItem
               title="Мойки и смесители"
-              href="https://userogue.com"
+              href="/catalog"
               src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
               description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
             />
