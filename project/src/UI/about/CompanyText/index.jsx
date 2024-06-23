05ecbdaa7fc076d1e5a61c1a2b68ff36397740f8
Eugene Che <indexpro24@gmail.com>
2024-06-23 12:39:11 +0300

    с

--- project/src/UI/about/CompanyText/index.jsx
+++ project/src/UI/about/CompanyText/index.jsx
@@ -1,10 +1,20 @@
 export default () => {
     return (
-        <div className="bg-white py-24 sm:py-32">
+        <div className="bg-white py-12 sm:py-24">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <div className="mx-auto max-w-3xl sm:text-center">
+                    <h2 className="text-base font-semibold leading-7 text-sky-600">О компании</h2>
+                    <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Приглашаем к сотрудничеству</p>
+                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                        Мы открыты для рассмотрения деловых предложений о взаимовыгодном сотрудничестве с мебельными компаниями
+                    </p>
+                </div>
+            </div>
+
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-                    <p className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</p>
-                    <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</h1>
+                    {/* <p className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</p>
+                    <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</h1> */}
                     <div className="mt-10 grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-700 lg:max-w-none lg:grid-cols-2">
                         <div>
                             <p>
--- project/src/UI/partnership/index.jsx
+++ project/src/UI/partnership/index.jsx
@@ -42,13 +42,13 @@ const features = [
 
 export default () => {
     return (
-        <div className="bg-white py-24 sm:py-32">
+        <div className="bg-white py-12 sm:py-24">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-3xl sm:text-center">
-                    <h2 className="text-base font-semibold leading-7 text-sky-600">Everything you need</h2>
+                    <h2 className="text-base font-semibold leading-7 text-sky-600">Партнёрство</h2>
                     <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Приглашаем к сотрудничеству</p>
                     <p className="mt-6 text-lg leading-8 text-gray-600">
-                        Наша компания открыта для рассмотрения предложений о взаимовыгодном сотрудничестве с торговыми компаниями, поставщиками, дизайнерами и с мебельными фабриками
+                        Мы открыты для рассмотрения деловых предложений о взаимовыгодном сотрудничестве с мебельными компаниями
                     </p>
                 </div>
             </div>
