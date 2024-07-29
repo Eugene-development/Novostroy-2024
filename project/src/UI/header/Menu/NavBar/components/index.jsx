fbe750005a2178bbb62a1c4833b027458a5d71a3
Eugene Che <indexpro24@gmail.com>
2024-07-29 22:00:09 +0300

    c

--- project/src/UI/header/Menu/NavBar/components/index.jsx
+++ project/src/UI/header/Menu/NavBar/components/index.jsx
@@ -102,7 +102,7 @@ export const ProductItem = ({ setActive, title, description, href, src }) => {
     <Link
       href={href}
       onClick={() => setActive(null)}
-      className="flex space-x-4 hover:scale110"
+      className="flex space-x-4 transition ease-in-out hover:scale-110"
     >
       <Image
         src={src}
