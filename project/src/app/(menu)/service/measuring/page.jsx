8d04e945f4eaff203e360efb38745aaacd0d0517
Eugene Che <indexpro24@gmail.com>
2024-07-29 12:55:50 +0300

    c

--- project/src/app/(menu)/service/measuring/page.jsx
+++ project/src/app/(menu)/service/measuring/page.jsx
@@ -1,7 +1,38 @@
 import { Service } from "@/UI"
+import { ArrowPathIcon, CloudArrowUpIcon, FingerPrintIcon, LockClosedIcon } from '@heroicons/react/24/outline'
+
+const data = {
+  name: "Замер помещения",
+  features: [
+    {
+      name: 'Выезд на адрес',
+      description:
+        'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+      icon: CloudArrowUpIcon,
+    },
+    {
+      name: 'По телефону',
+      description:
+        'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
+      icon: LockClosedIcon,
+    },
+    {
+      name: 'Через мессенджер',
+      description:
+        'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+      icon: ArrowPathIcon,
+    },
+    {
+      name: 'В салоне',
+      description:
+        'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+      icon: FingerPrintIcon,
+    },
+  ]
+}
 
 export default () => {
   return (
-    <Service />
+    <Service data={ data } />
   )
 }
