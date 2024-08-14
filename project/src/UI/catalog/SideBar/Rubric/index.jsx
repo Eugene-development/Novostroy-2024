10e75678aafa843794f61b1e9d57d29b97c4b017
Eugene Che <indexpro24@gmail.com>
2024-08-14 09:03:54 +0300

    c

--- project/src/UI/catalog/SideBar/Rubric/index.jsx
+++ project/src/UI/catalog/SideBar/Rubric/index.jsx
@@ -68,33 +68,56 @@ export default function Index({ dataCatalog }) {
         ))}
       </ul>
 
-      <ul className="pt-5 mt-5 space-y-2 border-t border-gray-200 dark:border-gray-700">
-        <li>
-          <a
-            href="#"
-            className="flex items-center p-1 text-sm font-normal tracking-wide text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-700 group"
+      <ul className="py-6 space-y-2 border-t border-gray-200 dark:border-gray-700">
+      <li>
+        <Link
+          href="/action"
+          className="flex items-center p-1 text-sm font-normal tracking-wide text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-700 group"
+        >
+          <svg
+            className="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
           >
-            <svg
-              className="flex-shrink-0 w-6 h-6 text-gray-400  transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                clipRule="evenodd"
-                fillRule="evenodd"
-                d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-              ></path>
-            </svg>
-            <span className="flex-1 ml-3 whitespace-nowrap">
-              Спецпредложения
-            </span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
+            <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"></path>
+            <path
+              clipRule="evenodd"
+              fillRule="evenodd"
+              d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
+            ></path>
+          </svg>
+          <span className="flex-1 ml-3 whitespace-nowrap">Акции</span>
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/contacts"
+          className="flex items-center p-1 text-sm font-normal tracking-wide text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-700 group"
+        >
+          <svg
+            className="flex-shrink-0 w-6 h-6 text-gray-400  transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              clipRule="evenodd"
+              fillRule="evenodd"
+              d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+            ></path>
+          </svg>
+          <span className="flex-1 ml-3 whitespace-nowrap">
+            Контакты
+          </span>
+        </Link>
+      </li>
+
+      <li>
+          <Link
+            href="/favorites"
             className="flex items-center p-1 text-sm font-normal tracking-wide text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-700 group"
           >
             <svg
@@ -111,52 +134,33 @@ export default function Index({ dataCatalog }) {
               ></path>
             </svg>
             <span className="flex-1 ml-3 whitespace-nowrap">Избранное</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="flex items-center p-1 text-sm font-normal tracking-wide text-gray-900 rounded-lg dark:text-white hover:bg-gray-50 dark:hover:bg-gray-700 group"
-          >
-            <svg
-              className="flex-shrink-0 w-6 h-6 text-gray-400 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"></path>
-              <path
-                clipRule="evenodd"
-                fillRule="evenodd"
-                d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"
-              ></path>
-            </svg>
-            <span className="flex-1 ml-3 whitespace-nowrap">Акции</span>
-          </a>
-        </li>
-        <li>
-          <Link
-            href="/"
-            className="flex items-center p-1 text-sm font-normal text-red-800 rounded-lg dark:text-red-500 hover:bg-gray-50 dark:hover:bg-gray-700 group"
-          >
-            <svg
-              className="flex-shrink-0 w-6 h-6 text-red-800 transition duration-75 dark:text-red-500"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-            >
-              <path
-                clipRule="evenodd"
-                fillRule="evenodd"
-                d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-              ></path>
-            </svg>
-            <span className="flex-1 ml-3 whitespace-nowrap">На главную</span>
           </Link>
         </li>
+
       </ul>
+      
+      <div className="py-6 border-t border-gray-200">
+        <Link
+          href="/"
+          className="flex items-center p-1 text-sm font-normal text-red-800 rounded-lg dark:text-red-500 hover:bg-gray-50 dark:hover:bg-gray-700 group"
+        >
+          <svg
+            className="flex-shrink-0 w-6 h-6 text-red-800 transition duration-75 dark:text-red-500"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              clipRule="evenodd"
+              fillRule="evenodd"
+              d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
+            ></path>
+          </svg>
+          <span className="flex-1 ml-3 whitespace-nowrap">На главную</span>
+        </Link>
+      </div>
+
     </Fragment>
   );
 }
