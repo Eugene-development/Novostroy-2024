49ea2c17c2cfba592a3c11e0eec6b93f65077f87
Eugene Che <indexpro24@gmail.com>
2024-08-24 13:35:32 +0300

    c

--- project/src/UI/catalog/SideBar/Rubric/index.jsx
+++ project/src/UI/catalog/SideBar/Rubric/index.jsx
@@ -67,7 +67,7 @@ export default function Index({ dataCatalog }) {
                         >
                           {subitem.value}
                         </Link>
-                        <span className="mr-4 inline-flex justify-center items-center w-5 h-5 text-xs font-semibold rounded-full text-primary-800 bg-primary-100 dark:bg-primary-200 dark:text-primary-800">
+                        <span className="mr-4 inline-flex justify-center items-center w-5 h-5 text-xs font-semibold rounded-full text-primary-800 bg-primary-50 dark:bg-primary-200 dark:text-primary-800">
                           {subitem.product_count}
                         </span>
 
