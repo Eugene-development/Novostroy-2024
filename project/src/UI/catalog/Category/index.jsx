a51a896dd7a02e889d99e000def049d2bfdf86f0
Eugene Che <indexpro24@gmail.com>
2024-09-06 18:10:56 +0300

    c

--- project/src/UI/catalog/Category/index.jsx
+++ project/src/UI/catalog/Category/index.jsx
@@ -1,7 +1,5 @@
-import { BreadCrumbs } from "@/UI";
+import { BreadCrumbs, Card } from "@/UI";
 
-import Card from "./Card";
-import Pagination from "./Pagination";
 export default ({ dataCategory }) => {
   return (
     <section className="bg-gray-50 py-2 antialiased dark:bg-gray-900 md:py-4 rounded-xl">
--- project/src/UI/index.js
+++ project/src/UI/index.js
@@ -51,6 +51,8 @@ export { default as Search } from "./catalog/Search";
 export { default as ProductDetail } from "./catalog/ProductDetail";
 export { default as OrderInfo } from "./catalog/OrderInfo";
 export { default as Filter } from "./catalog/Filter";
+export { default as Pagination } from "./catalog/Pagination";
+export { default as Card } from "./catalog/Card";
 
 // Service
 export { default as Service } from "./service";
