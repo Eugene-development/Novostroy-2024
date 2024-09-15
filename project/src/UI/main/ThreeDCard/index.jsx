410f99a1e359d3d07871791fb58eca1d36e79e33
Eugene Che <indexpro24@gmail.com>
2024-09-15 21:38:10 +0300

    c

--- project/src/UI/main/ThreeDCard/index.jsx
+++ project/src/UI/main/ThreeDCard/index.jsx
@@ -5,6 +5,18 @@ import React from "react";
 import { CardBody, CardContainer, CardItem } from "./components";
 import Link from "next/link";
 
+const data = [
+  {
+    name: "Кухни"
+  },
+  {
+    name: "Шкафы"
+  },
+  {
+    name: "Гардеробы"
+  },
+]
+
 export default function ThreeDCardDemo() {
   return (
     <>
@@ -19,9 +31,9 @@ export default function ThreeDCardDemo() {
           </div>
         </div>
         <div className="text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-7xl">
+          <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-7xl">
             Мебель для дома
-          </h1>
+          </h2>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             Наша компания предлагает вам мебель, спроектированную лучшими
             дизайнерами и сделанную лучшими фабриками от эконом сегмента до
@@ -30,14 +42,15 @@ export default function ThreeDCardDemo() {
         </div>
       </div>
 
-      <div className="flex items-center justify-center space-x-8">
-        <CardContainer className="inter-var">
-          <CardBody className="bg-gray-50 relative group/card hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-12 border  ">
+      <div className="md:flex items-center justify-center">
+      {data.map((item) => (
+        <CardContainer key={item.name} className="mx-4">
+          <CardBody className="bg-gray-50 relative  hover:shadow-2xl  border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-12 border  ">
             <CardItem
               translateZ="50"
               className="text-2xl font-bold text-neutral-600 dark:text-white"
             >
-              Кухни
+              {item.name}
             </CardItem>
             <CardItem
               as="p"
@@ -71,101 +84,14 @@ export default function ThreeDCardDemo() {
                 as="button"
                 className=" px-4 py-2 rounded-xl bg-gray-800 dark:bg-white dark:text-black text-white text-xs font-bold"
               >
-                Бесплатная консультация
+                Консультация
               </CardItem>
             </div>
           </CardBody>
         </CardContainer>
-        <CardContainer className="inter-var">
-          <CardBody className="bg-gray-50 relative group/card hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-12 border  ">
-            <CardItem
-              translateZ="50"
-              className="text-2xl font-bold text-neutral-600 dark:text-white"
-            >
-              Шкафы
-            </CardItem>
-            <CardItem
-              as="p"
-              translateZ="60"
-              className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-            >
-              Осуществим проектный замер помещения в проекции. Стоимость услуги
-              компенсируется при оформлении договора.
-            </CardItem>
-            <CardItem translateZ="100" className="w-full mt-8">
-              <Image
-                src="https://storage.yandexcloud.net/mine2024/zov/4DCcVnbk5suIuE8pD7yLzC6CUReEjgNJZkD2c32S.png"
-                height="1000"
-                width="1000"
-                className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                alt="thumbnail"
-              />
-            </CardItem>
-            <div className="flex justify-between items-center mt-20">
-              <CardItem
-                translateZ={20}
-                as={Link}
-                href="https://twitter.com/mannupaaji"
-                target="__blank"
-                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-              >
-                Подробнее →
-              </CardItem>
-              <CardItem
-                translateZ={20}
-                as="button"
-                className="px-4 py-2 rounded-xl bg-gray-800 dark:bg-white dark:text-black text-white text-xs font-bold"
-              >
-                Бесплатная консультация
-              </CardItem>
-            </div>
-          </CardBody>
-        </CardContainer>
-        <CardContainer className="inter-var">
-          <CardBody className="bg-gray-50 relative group/card  hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-12 border  ">
-            <CardItem
-              translateZ="50"
-              className="text-2xl font-bold text-neutral-600 dark:text-white"
-            >
-              Гардеробы
-            </CardItem>
-            <CardItem
-              as="p"
-              translateZ="60"
-              className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-            >
-              Дизайнер составит проект мебели с учётом ваших пожеланий, бюджета,
-              размеров помещения и своего проектного опыта.
-            </CardItem>
-            <CardItem translateZ="100" className="w-full mt-8">
-              <Image
-                src="https://storage.yandexcloud.net/mine2024/zov/4DCcVnbk5suIuE8pD7yLzC6CUReEjgNJZkD2c32S.png"
-                height="1000"
-                width="1000"
-                className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-                alt="thumbnail"
-              />
-            </CardItem>
-            <div className="flex justify-between items-center mt-20">
-              <CardItem
-                translateZ={20}
-                as={Link}
-                href="https://twitter.com/mannupaaji"
-                target="__blank"
-                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-              >
-                Подробнее →
-              </CardItem>
-              <CardItem
-                translateZ={20}
-                as="button"
-                className="px-4 py-2 rounded-xl bg-gray-800 dark:bg-white dark:text-black text-white text-xs font-bold"
-              >
-                Бесплатная консультация
-              </CardItem>
-            </div>
-          </CardBody>
-        </CardContainer>
+
+        
+      ))}
       </div>
     </>
   );
