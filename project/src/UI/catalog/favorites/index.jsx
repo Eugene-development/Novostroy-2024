f9572f8da52f76d9b0fd02f9a48efb07af45fb6f
Eugene Che <indexpro24@gmail.com>
2024-09-15 11:30:42 +0300

    c

--- project/src/UI/catalog/favorites/index.jsx
+++ project/src/UI/catalog/favorites/index.jsx
@@ -8,7 +8,7 @@ import { useState, useEffect } from "react";
 import { useFavoritesProductsStore } from "@/stores/favorites";
 const { favoritesProducts } = useFavoritesProductsStore;
 
-export default ({ products }) => {
+export default () => {
   const router = useRouter();
 
   const { currentFavoritesProducts } = favoritesProducts();
@@ -23,90 +23,124 @@ export default ({ products }) => {
     router.back();
   };
 
-  return (
-    domLoaded && (
-      <section className="bg-gray-50 py-8 antialiased dark:bg-gray-900 md:py-16">
-        <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
-          <div className="mb-4 flex items-center justify-between gap-4 md:mb-8">
-            <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
-              Избранное
-            </h2>
+  return domLoaded && currentFavoritesProducts.length > 0 ? (
+    <section className="bg-gray-50 py-8 antialiased dark:bg-gray-900 md:py-16">
+      <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+        <div className="mb-4 flex items-center justify-between gap-4 md:mb-8">
+          <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+            Избранное
+          </h2>
 
-            <button
-              onClick={handleBack}
-              className="flex text-base font-medium text-gray-900 bg-gray-100 hover:bg-gray-200 px-4 py-1 ring-2 ring-gray-200 rounded-lg"
+          <button
+            onClick={handleBack}
+            className="flex text-base font-medium text-gray-900 bg-gray-100 hover:bg-gray-200 px-4 py-1 ring-2 ring-gray-200 rounded-lg"
+          >
+            {" "}
+            <span className="mr-2">Вернуться </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5"
             >
-              {" "}
-              <span className="mr-2">Вернуться </span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
-                />
-              </svg>
-            </button>
-          </div>
-
-          <div className="grid grid-cols-1 gap-4 sm:mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:gap-8">
-            {currentFavoritesProducts.map((item) => (
-              <div
-                key={item.slug}
-                className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800"
-              >
-                <Image
-                  className="mx-auto mb-4 h-60 dark:hidden md:mb-6"
-                  src={`${process.env.NEXT_PUBLIC_S3}/${item?.image[0]?.hash}`}
-                  alt={item.value}
-                  width={600}
-                  height={600}
-                />
-
-                <Link
-                  href={`/${item.parentable.parentable.parentable.slug}/${item.parentable.parentable.slug}/${item.parentable.slug}`}
-                  className="inline-flex items-center bg-gray-50 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300"
-                >
-                  Категория - {item.parentable.value}
-                </Link>
-
-                <span className="mt-4 block font-medium text-gray-900  dark:text-white">
-                  {item.value}
-                </span>
-
-                <Link
-                  href={`/${item.parentable.parentable.parentable.slug}/${item.slug}`}
-                  title=""
-                  className="mt-4 inline-flex items-center gap-1.5 font-medium text-sky-700 hover:text-sky-600 dark:text-sky-500 dark:hover:text-sky-400"
-                >
-                  Подробнее
-                  <svg
-                    className="h-5 w-5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M19 12H5m14 0-4 4m4-4-4-4"
-                    ></path>
-                  </svg>
-                </Link>
-              </div>
-            ))}
-          </div>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+              />
+            </svg>
+          </button>
         </div>
-      </section>
-    )
+
+        <div className="grid grid-cols-1 gap-4 sm:mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:gap-8">
+          {currentFavoritesProducts.map((item) => (
+            <div
+              key={item.slug}
+              className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800"
+            >
+              <Image
+                className="mx-auto mb-4 h-60 dark:hidden md:mb-6"
+                src={`${process.env.NEXT_PUBLIC_S3}/${item?.image[0]?.hash}`}
+                alt={item.value}
+                width={600}
+                height={600}
+              />
+
+              <Link
+                href={`/${item.parentable.parentable.parentable.slug}/${item.parentable.parentable.slug}/${item.parentable.slug}`}
+                className="inline-flex items-center bg-gray-50 text-red-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-300"
+              >
+                Категория - {item.parentable.value}
+              </Link>
+
+              <span className="mt-4 block font-medium text-gray-900  dark:text-white">
+                {item.value}
+              </span>
+
+              <Link
+                href={`/${item.parentable.parentable.parentable.slug}/${item.slug}`}
+                title=""
+                className="mt-4 inline-flex items-center gap-1.5 font-medium text-sky-700 hover:text-sky-600 dark:text-sky-500 dark:hover:text-sky-400"
+              >
+                Подробнее
+                <svg
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M19 12H5m14 0-4 4m4-4-4-4"
+                  ></path>
+                </svg>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  ) : (
+    <section className="bg-gray-50 py-8 antialiased dark:bg-gray-900 md:py-16">
+      <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+        <div className="mb-4 flex items-center justify-between gap-4 md:mb-8">
+          <h2 className="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
+            Избранное
+          </h2>
+
+          <button
+            onClick={handleBack}
+            className="flex text-base font-medium text-gray-900 bg-gray-100 hover:bg-gray-200 px-4 py-1 ring-2 ring-gray-200 rounded-lg"
+          >
+            {" "}
+            <span className="mr-2">Вернуться </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+              />
+            </svg>
+          </button>
+        </div>
+
+        <div className="grid grid-cols-1 gap-4 sm:mt-8 sm:grid-cols-2 lg:grid-cols-3 xl:gap-8">
+          В разделе пока пусто
+        </div>
+      </div>
+    </section>
   );
 };
--- project/src/UI/header/Banner/index.jsx
+++ project/src/UI/header/Banner/index.jsx
@@ -5,127 +5,15 @@ import Search from "./Search";
 import Favorites from "./Favorites";
 import { PhoneIcon, EnvelopeIcon } from "@heroicons/react/20/solid";
 
-export default (props) => {
+export default () => {
   return (
-    <div className=" relative isolate flex items-center py-1 px-4 sm:px-3.5 bg-gray-950  justify-between">
-      {/* <svg
-        viewBox="0 0 577 310"
-        aria-hidden="true"
-        className="absolute top-1/2 left-[max(-7rem,calc(30%-12rem))] -z-10 w-[20rem] -translate-y-1/2 transform-gpu blur-2xl"
-      >
-        <path
-          id="a906133b-f855-4023-a54c-38d70c72fe9c"
-          fill="url(#be47b6c9-9c22-49b2-a209-168b52fa0ada)"
-          fillOpacity=".3"
-          d="m142.787 168.697-75.331 62.132L.016 88.702l142.771 79.995 135.671-111.9c-16.495 64.083-23.088 173.257 82.496 97.291C492.935 59.13 494.936-54.366 549.339 30.385c43.523 67.8 24.892 159.548 10.136 196.946l-128.493-95.28-36.628 177.599-251.567-140.953Z"
-        />
-        <defs>
-          <linearGradient
-            id="be47b6c9-9c22-49b2-a209-168b52fa0ada"
-            x1="614.778"
-            x2="-42.453"
-            y1="26.617"
-            y2="96.115"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#9089FC" />
-            <stop offset={1} stopColor="#FF80B5" />
-          </linearGradient>
-        </defs>
-      </svg> */}
-      <div className=" flex flex-1 justify-start">
-        <div className="hidden md:flex space-x-4 items-center justify-center">
-          <Link href="/about" className="ml-1 text-sm text-gray-50">
-            О компании
-          </Link>
-          <svg
-            viewBox="0 0 2 2"
-            className="mx-2 inline h-0.5 w-0.5 fill-current text-sky-500"
-            aria-hidden="true"
-          >
-            <circle cx={1} cy={1} r={1} />
-          </svg>
-          <Link href="/partnership" className="ml-1 text-sm text-gray-50">
-            Партнёрство
-          </Link>
-          <svg
-            viewBox="0 0 2 2"
-            className="mx-2 inline h-0.5 w-0.5 fill-current text-sky-500"
-            aria-hidden="true"
-          >
-            <circle cx={1} cy={1} r={1} />
-          </svg>
-          <Link href="/testimonials" className="ml-1 text-sm text-gray-50">
-            Отзывы
-          </Link>
-          <svg
-            viewBox="0 0 2 2"
-            className="mx-2 inline h-0.5 w-0.5 fill-current text-sky-500"
-            aria-hidden="true"
-          >
-            <circle cx={1} cy={1} r={1} />
-          </svg>
-          <Link href="/installment" className="ml-1 text-sm text-gray-50">
-            Рассрочка
-          </Link>
-          <svg
-            viewBox="0 0 2 2"
-            className="mx-2 inline h-0.5 w-0.5 fill-current text-sky-500"
-            aria-hidden="true"
-          >
-            <circle cx={1} cy={1} r={1} />
-          </svg>
-          <Link href="/contacts" className="ml-1 text-sm text-gray-50">
-            Контакты
-          </Link>
-        </div>
-        {/* <div className="hidden md:flex">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-5 h-5 text-indigo-300"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-            />
-          </svg>
-          <span className="ml-1 text-sm text-gray-50">Москва</span>
-        </div> */}
-
-        <div className="flex md:hidden">
-          <EnvelopeIcon
-            className="mt-0.5 h-5 w-5 text-indigo-100"
-            aria-hidden="true"
-          />
-          <span className="ml-2 text-sm text-gray-50">info@novostroy.org</span>
-        </div>
-      </div>
-
-      <div className="hidden md:flex flex-1 justify-end ">
-        <div className="hidden 2xl:flex 2xl:flex-1 ">
-          <div className="py-1.5 lg:flex lg:min-w-0 lg:flex-1 lg:justify-end items-center">
-            <div className=" bg-gray-950 flex items-center justify-center rounded-lg px-2 xl:px-4 py-1 text-xl font-semibold leading-6  shadow-lg shadow-sky-950/50  ring-offset-2  ring-gray-200">
-              <PhoneIcon
-                className="block size-3 xl:size-4 text-gray-50 "
-                aria-hidden="true"
-              />
-              <Link
-                href="tel:+79154000020"
-                className=" ml-2 text-sm font-normal text-gray-50"
-              >
-                8 (915) 400-00-20
-              </Link>
-            </div>
+    <>
+      <div className="  hidden md:flex items-center py-1 px-4 sm:px-3.5 bg-gray-900 ">
+        <div className=" flex flex-1 justify-start">
+          <div className="flex space-x-4 items-center justify-center">
+            <Link href="/about" className="ml-1 text-sm text-gray-50">
+              О компании
+            </Link>
             <svg
               viewBox="0 0 2 2"
               className="mx-2 inline h-0.5 w-0.5 fill-current text-sky-500"
@@ -133,35 +21,89 @@ export default (props) => {
             >
               <circle cx={1} cy={1} r={1} />
             </svg>
-            <div className=" bg-gray-950 flex items-center justify-center rounded-lg px-2 xl:px-4 py-1 text-xl font-semibold leading-6  shadow-lg shadow-sky-950/50  ring-offset-2  ring-gray-200">
-              <EnvelopeIcon
-                className="block size-3 xl:size-4 text-gray-50 "
-                aria-hidden="true"
-              />
-              <div className="ml-2 text-sm font-normal text-gray-50">
-                info@novostroy.org
+            <Link href="/partnership" className="ml-1 text-sm text-gray-50">
+              Партнёрство
+            </Link>
+            <svg
+              viewBox="0 0 2 2"
+              className="mx-2 inline h-0.5 w-0.5 fill-current text-sky-500"
+              aria-hidden="true"
+            >
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            <Link href="/testimonials" className="ml-1 text-sm text-gray-50">
+              Отзывы
+            </Link>
+            <svg
+              viewBox="0 0 2 2"
+              className="mx-2 inline h-0.5 w-0.5 fill-current text-sky-500"
+              aria-hidden="true"
+            >
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            <Link href="/installment" className="ml-1 text-sm text-gray-50">
+              Рассрочка
+            </Link>
+            <svg
+              viewBox="0 0 2 2"
+              className="mx-2 inline h-0.5 w-0.5 fill-current text-sky-500"
+              aria-hidden="true"
+            >
+              <circle cx={1} cy={1} r={1} />
+            </svg>
+            <Link href="/contacts" className="ml-1 text-sm text-gray-50">
+              Контакты
+            </Link>
+          </div>
+        </div>
+
+        <div className="hidden md:flex flex-1 justify-between ">
+          <div className="hidden 2xl:flex 2xl:flex-1 ">
+            <div className="py-1.5 lg:flex lg:min-w-0 lg:flex-1 lg:justify-end items-center">
+              <div className=" bg-gray-950 flex items-center justify-center rounded-lg px-2 xl:px-4 py-1 text-xl font-semibold leading-6  shadow-lg shadow-sky-950/50  ring-offset-2  ring-gray-200">
+                {/*<PhoneIcon
+                  className="block size-3 xl:size-4 text-gray-50 "
+                  aria-hidden="true"
+                />*/}
+                <Link
+                  href="tel:+79154000020"
+                  className="text-sm font-normal text-gray-50"
+                >
+                  8 (915) 400-00-20
+                </Link>
               </div>
+              <svg
+                viewBox="0 0 2 2"
+                className="mx-2 inline h-0.5 w-0.5 fill-current text-sky-500"
+                aria-hidden="true"
+              >
+                <circle cx={1} cy={1} r={1} />
+              </svg>
+              <div className=" bg-gray-950 flex items-center justify-center rounded-lg px-2 xl:px-4 py-1 text-xl font-semibold leading-6  shadow-lg shadow-sky-950/50  ring-offset-2  ring-gray-200">
+                <div className=" text-sm font-normal text-gray-50">
+                  info@novostroy.org
+                </div>
+              </div>
+              <svg
+                viewBox="0 0 2 2"
+                className="mx-2 inline h-0.5 w-0.5 fill-current text-sky-500"
+                aria-hidden="true"
+              >
+                <circle cx={1} cy={1} r={1} />
+              </svg>
+
+              <Favorites />
             </div>
-            <svg
-              viewBox="0 0 2 2"
-              className="mx-2 inline h-0.5 w-0.5 fill-current text-sky-500"
-              aria-hidden="true"
-            >
-              <circle cx={1} cy={1} r={1} />
-            </svg>
           </div>
         </div>
       </div>
-      <div className="flex divide-x-2 divide-sky-800">
+      <div className="md:hidden flex justify-between bg-gray-900">
         <Favorites />
-      </div>
 
-      <div className="md:hidden flex justify-end">
-        <PhoneIcon className="h-5 w-5 text-indigo-100" aria-hidden="true" />
         <Link href="tel:+79154000020" className="ml-1 text-base text-gray-50">
           8 (915) 400-00-20
         </Link>
       </div>
-    </div>
+    </>
   );
 };
