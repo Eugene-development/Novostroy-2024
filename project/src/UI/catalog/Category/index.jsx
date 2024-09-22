0c8f3b89db44e73a3d7359a26f7220751281b633
Eugene Che <indexpro24@gmail.com>
2024-09-22 08:44:57 +0300

    c

--- project/src/UI/catalog/Category/index.jsx
+++ project/src/UI/catalog/Category/index.jsx
@@ -2,8 +2,8 @@ import { BreadCrumbs, Card } from "@/UI";
 
 export default ({ dataCategory }) => {
   return (
-    <section className="bg-gray-50 py-2 antialiased dark:bg-gray-900 md:py-4 rounded-xl">
-      <div className="mx-auto max-w-screen-xl ">
+    <section className="bg-gray-50 py-2 antialiased md:py-4 rounded-xl">
+      <div className="mx-auto max-w-screen-xl">
         <BreadCrumbs data={dataCategory} />
 
         <Card dataCategory={dataCategory} />
