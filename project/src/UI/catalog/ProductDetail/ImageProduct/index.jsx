a9bc65c72171a63d40816ab68a2f79c5e0205e1a
Eugene Che <indexpro24@gmail.com>
2024-09-27 21:03:50 +0300

    c

--- project/src/UI/catalog/ProductDetail/ImageProduct/index.jsx
+++ project/src/UI/catalog/ProductDetail/ImageProduct/index.jsx
@@ -4,17 +4,17 @@ import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 
 export default ({ dataProduct }) => {
   return (
-    <div className="mx-auto max-w-2xl px-1    lg:max-w-8xl ">
+    <div className="mx-auto max-w-2xl px-1 lg:max-w-8xl">
       <div className=" lg:items-start ">
         {/* Image gallery */}
         <TabGroup className="flex flex-col-reverse">
           {/* Image selector */}
-          <div className="mx-auto mt-6 hidden w-[40rem] max-w-7xl sm:block ">
-            <TabList className="grid grid-cols-4 gap-6">
+          <div className="mx-auto mt-6 w-[20rem] sm:w-[40rem] max-w-7xl sm:block ">
+            <TabList className="grid grid-cols-4 gap-2 sm:gap-6">
               {dataProduct.image.map(({ hash }) => (
                 <Tab
                   key={hash}
-                  className="group relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
+                  className="group relative flex h-12 sm:h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
                 >
                   <span className="absolute inset-0 overflow-hidden rounded-md ">
                     <Image
@@ -39,7 +39,7 @@ export default ({ dataProduct }) => {
               <TabPanel key={hash}>
                 <Image
                   src={`${process.env.NEXT_PUBLIC_S3}/${hash}`}
-                  className="h-full w-full  object-cover object-center sm:rounded-lg "
+                  className="h-full w-full  object-cover object-center rounded-lg "
                   alt="image"
                   width={640}
                   height={320}
--- project/src/UI/catalog/ProductDetail/index.jsx
+++ project/src/UI/catalog/ProductDetail/index.jsx
@@ -5,13 +5,13 @@ import Product from "./Product";
 
 export default ({ dataProduct }) => {
   return (
-    <section className="bg-gray-50 py-2  sm:px-0 antialiased md:py-4 rounded-xl">
+    <section className="sm:bg-gray-50 py-2 sm:px-0 antialiased md:py-4 rounded-xl">
       <div className="mx-auto max-w-screen-xl">
         <BreadCrumbs data={dataProduct} />
         <section className="py-1 bg-white md:py-2 xl:py-8 dark:bg-gray-900 antialiased rounded-lg">
           <div className="max-w-screen-xl mx-auto">
             <div className="lg:flex justify-between">
-              <div className="px-8">
+              <div className="sm:px-8">
                 <ImageProduct dataProduct={dataProduct} />
                 {/* <Accordion /> */}
               </div>
