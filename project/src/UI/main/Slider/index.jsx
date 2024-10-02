f12628aef7849774f8863852c37c551aea689227
Eugene Che <indexpro24@gmail.com>
2024-10-02 21:48:35 +0300

    c

--- project/src/UI/main/Slider/index.jsx
+++ project/src/UI/main/Slider/index.jsx
@@ -30,29 +30,27 @@ export default function ImagesSliderDemo() {
         }}
         className="-mt-24 z-50 flex flex-col justify-center items-center"
       >
-        <p className="font-bold text-3xl md:text-8xl text-center bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-400 py-4">
-           Тест! Сайт в разработке!
-        </p>
+        
         <p className="font-bold text-3xl md:text-8xl text-center bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-400 py-4">
           <span className="text-4xl md:text-8xl">Мы делаем</span> <br />
           вашу жизнь уютнее
         </p>
         <p className="md:hidden px-4 max-w-4xl text-center font-extralight xl:text-lg text-gray-100 tracking-wide">
-        Наша команда это профессиональные дизайнеры, работающие в лучших
-        мебельных компаниях.
+        Наша команда это профессиональные дизайнеры, работающие в лучших известных
+        мебельных компаниях. <br /> Наши услуги бесплатны.
         </p>
         <p className="hidden md:block max-w-4xl text-center font-extralight xl:text-lg text-gray-100 tracking-wide">
           Наша команда это профессиональные дизайнеры, работающие в лучших
           мебельных компаниях. Мы предложим Вам различные варианты вашего
           мебельного проекта с оптимальной ценой и отличным качеством
-          исполнения. Наши услуги бесплатны.
+          исполнения. <br /> Наши услуги бесплатны.
         </p>
         <button
           onClick={openVisibleFormDesigner}
-          className="scale110 mt-8 px-8 py-2 sm:py-4 backdrop-blur-sm border bg-sky-300/10 border-sky-500/20 text-white mx-auto text-center rounded-full relative"
+          className="scale110 mx-4 mt-8 px-8 py-2 sm:py-4 backdrop-blur-sm border bg-sky-300/10 border-sky-500/20 text-white  text-center rounded-full relative"
         >
           <span className="text-sm sm:text-lg mt-6">
-            Бесплатная консультация дизайнера →
+            Консультация дизайнера →
           </span>
           <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-sky-500 to-transparent" />
         </button>
